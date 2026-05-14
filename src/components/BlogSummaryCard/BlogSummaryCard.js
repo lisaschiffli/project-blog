cc9698a23c007a2622a306afa980d601577ab4b5
lisaschiffli <lisaschiffli@users.noreply.github.com>
2026-05-14 12:15:32 -0400

    Finished exercise 1

--- src/components/BlogSummaryCard/BlogSummaryCard.js
+++ src/components/BlogSummaryCard/BlogSummaryCard.js
@@ -6,12 +6,7 @@ import Card from '@/components/Card';
 
 import styles from './BlogSummaryCard.module.css';
 
-function BlogSummaryCard({
-  slug,
-  title,
-  publishedOn,
-  abstract,
-}) {
+function BlogSummaryCard({ slug, title, publishedOn, abstract }) {
   const href = `/${slug}`;
   const humanizedDate = format(
     new Date(publishedOn),
@@ -26,12 +21,8 @@ function BlogSummaryCard({
       <time dateTime={publishedOn}>{humanizedDate}</time>
       <p>
         {abstract}{' '}
-        <Link
-          href={href}
-          className={styles.continueReadingLink}
-        >
-          Continue reading{' '}
-          <span className={styles.arrow}>→</span>
+        <Link href={href} className={styles.continueReadingLink}>
+          Continue reading <span className={styles.arrow}>→</span>
         </Link>
       </p>
     </Card>
