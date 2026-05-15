fa958a61269327646d3aca419d9d4dc5989b337f
lisaschiffli <lisaschiffli@users.noreply.github.com>
2026-05-15 10:12:16 -0400

    Finished exercise 4

--- src/app/[postSlug]/page.js
+++ src/app/[postSlug]/page.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { MDXRemote } from 'next-mdx-remote/rsc';
 import { loadBlogPost } from '@/helpers/file-helpers';
 import { BLOG_TITLE } from '@/constants';
+import CodeSnippet from '@/components/CodeSnippet';
 
 import BlogHero from '@/components/BlogHero';
 
@@ -10,7 +11,7 @@ import styles from './postSlug.module.css';
 export async function generateMetadata({ params }) {
   const {
     frontmatter: { title, abstract },
-  } = await loadBlogPost(params.postSlug);
+  } = await loadBlogPost((await params).postSlug);
 
   return {
     title: `${title} • ${BLOG_TITLE}`,
@@ -22,7 +23,7 @@ async function BlogPost({ params }) {
   const {
     frontmatter: { title, publishedOn },
     content,
-  } = await loadBlogPost(params.postSlug);
+  } = await loadBlogPost((await params).postSlug);
 
   return (
     <article className={styles.wrapper}>
@@ -31,7 +32,12 @@ async function BlogPost({ params }) {
         publishedOn={publishedOn}
       />
       <div className={styles.page}>
-        <MDXRemote source={content} />
+        <MDXRemote
+          source={content}
+          components={{
+            pre: CodeSnippet,
+          }}
+        />
       </div>
     </article>
   );
